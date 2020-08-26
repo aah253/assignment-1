7b5917a03f6b1bbea761fc48d483aa8b288ce59f
aah253 <70031484+aah253@users.noreply.github.com>
2020-08-25 23:35:02 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,19 +3,11 @@
 using namespace std;
 
 int FindFibonacciIterative(int n) {
-  int previous = 1;
-  int current = 1;
-  for (int i = 2; i < n; i++) {
-    int sum = previous + current;
-    previous = current;
-    current = sum;
-  }
-  return current;
   
 }
 
 int FindFibonacciRecursive(int n) {
-  return  1;
+
 }
 
 int main() {
@@ -23,9 +15,7 @@ int main() {
   cout << "Enter the number of terms of series : ";
   cin >> x;
 
-  cout << "\nFibonnaci Series Number: " << FindFibonacciIterative(x) <<"\n";
-
-    cout << "\nFibonnaci Series Number: " << FindFibonacciRecursive(x) <<"\n";
+  cout << FindFibonacciIterative(x) << " : " << FindFibonacciRecursive(x) <<"\n";
      
    return 0;
 }
